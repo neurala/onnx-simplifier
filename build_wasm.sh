659a591c8a55917477b8c4a8bd3b3cc81ffb1fbb
Audrey MP <apratt@neurala.com>
2025-09-18 10:55:34 -0700

    Downgrade to 0.4.36, but keep forked changes (removal of parse_size)

--- build_wasm.sh
+++ build_wasm.sh
@@ -8,17 +8,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WITH_NODE_RAW_FS=${1:-OFF}
 
 cd $SCRIPT_DIR
-if which protoc ; then
-    PROTOC=$(which protoc)
-else
-    pushd third_party/onnxruntime/cmake/external/protobuf/cmake
-    mkdir -p build
-    cd build
-    cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -GNinja ..
-    ninja protoc
-    PROTOC=`pwd`/protoc
-    popd
-fi
+pushd third_party/onnxruntime/cmake/external/protobuf/cmake
+mkdir -p build
+cd build
+cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB_DEFAULT=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -GNinja ..
+ninja protoc
+PROTOC=`pwd`/protoc
+popd
 
 mkdir -p build-wasm-node-$WITH_NODE_RAW_FS
 cd build-wasm-node-$WITH_NODE_RAW_FS
